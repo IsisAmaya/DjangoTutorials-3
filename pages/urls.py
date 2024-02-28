19e6300c8b872f0607575b1280ab82cb8708a4c4
IsisAmaya <icamayaa@eafit.edu.co>
2024-02-28 09:57:41 -0500

    parte A de tutorial-08 completada

--- pages/urls.py
+++ pages/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('products/product-created', ProductCreatedView.as_view(), name='product-created'),
     path('products/create', ProductCreateView.as_view(), name='form'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+
 ]
